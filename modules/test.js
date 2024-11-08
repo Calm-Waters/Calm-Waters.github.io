884813d7a836f08fb8d41ee24cda7bcb2f8a2e1e
Calm-Waters <99409522+Calm-Waters@users.noreply.github.com>
2024-11-08 15:17:16 +0100

    Create test.js

---
+++ modules/test.js
@@ -0,0 +1,10 @@
+window.test_mod = `
+<style>
+
+</style>
+<div class="scene vbox centercontainer">
+	<h1>Test Page</h1>
+	<button onclick="loadPage('hub')">Return to HUB</button>
+    <button onclick="loadPage('login')">Exit</button>
+</div>
+`;
