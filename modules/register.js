c807934f2721a15084807e6ddd548717d3180bfc
Calm-Waters <99409522+Calm-Waters@users.noreply.github.com>
2024-11-09 19:18:53 +0100

    Update register.js

--- modules/register.js
+++ modules/register.js
@@ -1,1 +1,17 @@
-
+const moduleName = "register";
+window[moduleName + '_layout'] = `
+<div class="scene hbox centercontainer register">
+	<form id="loginForm" onsubmit="register(event)">
+		<div class="form-group">
+			<input type="text" id="email" name="email" placeholder="email" required>
+		</div>
+		<div class="form-group">
+			<input type="password" id="password" name="password" placeholder="password" required>
+		</div>
+		<div class="hbox centercontainer">
+			<button type="submit">Log In</button>
+			<button onclick="loadPage('hub')">Skip</button>
+		</div>
+	</form>
+</div>
+`;
