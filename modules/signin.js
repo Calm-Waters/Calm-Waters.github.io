ef89ea57221781221b96343babfedbd6754809e6
Calm-Waters <99409522+Calm-Waters@users.noreply.github.com>
2024-11-09 19:41:37 +0100

    Update signin.js

--- modules/signin.js
+++ modules/signin.js
@@ -1,6 +1,20 @@
 const moduleName = "signin";
 window[moduleName + '_layout'] = `
 <div class="scene hbox centercontainer signin">
-	Testing.
+	<form id="signinForm" onsubmit="signin(event)">
+		<div class="form-group">
+			<input type="text" id="email" name="email" placeholder="email" required>
+		</div>
+		<div class="form-group">
+			<input type="password" id="password" name="password" placeholder="password" required>
+		</div>
+ 		<div class="form-group">
+			<input type="password" id="repeat-password" name="repeat password" placeholder="prepeat assword" required>
+		</div>
+		<div class="hbox centercontainer">
+			<button type="submit">Sign In</button>
+			<button type="button" onclick="loadPage('login')">Return to Login</button>
+		</div>
+	</form>
 </div>
 `;
