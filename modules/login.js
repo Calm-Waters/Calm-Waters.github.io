502ae962f2c9a69fab4374f21020028e733ea066
Calm-Waters <99409522+Calm-Waters@users.noreply.github.com>
2024-11-09 15:28:33 +0100

    Update login.js

--- modules/login.js
+++ modules/login.js
@@ -3,9 +3,9 @@ window.login_layout = `
 
 </style>
 <div class="scene hbox centercontainer login">
-	<form id="loginForm">
+	<form id="loginForm" onsubmit="login(event)">
 		<div class="form-group">
-			<input type="text" id="username" name="username" placeholder="username" required>
+			<input type="text" id="email" name="email" placeholder="email" required>
 		</div>
 		<div class="form-group">
 			<input type="password" id="password" name="password" placeholder="password" required>
@@ -17,3 +17,34 @@ window.login_layout = `
 	</form>
 </div>
 `;
+const savedEmail = localStorage.getItem("email");
+const savedPassword = localStorage.getItem("password");
+if (savedEmail) { document.getElementById("email").value = savedEmail;}
+if (savedPassword) { document.getElementById("password").value = savedPassword;
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+auth = getAuth();
+function login(event) {
+	event.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    if (!email || !password) {
+	    alert("Please fill in both fields.");
+	    return;
+	}
+	try {
+	    signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				const user = userCredential.user;		
+				localStorage.setItem("email", email);
+	            localStorage.setItem("password", password);
+				loadPage('hub');
+			})
+				.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+			});
+	}	catch (error) {
+        console.error('Error logging in:', error.message);
+        alert("Error: " + error.message);  // Display error message to the user
+    }
+}
