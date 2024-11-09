46b678efaa9d21b457d1aaac4232c3e40d4b6373
Calm-Waters <99409522+Calm-Waters@users.noreply.github.com>
2024-11-09 14:53:38 +0100

    Update login.js

--- modules/login.js
+++ modules/login.js
@@ -4,17 +4,15 @@ window.login_mod = `
 </style>
 <div class="scene hbox centercontainer login">
 	<form id="loginForm">
-	    <div class="form-group">
-	        <!-- <label for="username">Username:</label> -->
-	        <input type="text" id="username" name="username" placeholder="username" required>
-	    </div>
-	    <div class="form-group">
-	        <!-- <label for="password">Password:</label> -->
-	        <input type="password" id="password" name="password" placeholder="password" required>
-	    </div>
-	    <div class="hbox centercontainer">
-		    <button type="submit">Log In</button>
-		    <button onclick="loadPage('hub')">Skip</button>
+		<div class="form-group">
+			<input type="text" id="username" name="username" placeholder="username" required>
+		</div>
+		<div class="form-group">
+			<input type="password" id="password" name="password" placeholder="password" required>
+		</div>
+		<div class="hbox centercontainer">
+			<button type="submit">Log In</button>
+			<button onclick="loadPage('hub')">Skip</button>
 		</div>
 	</form>
 </div>
