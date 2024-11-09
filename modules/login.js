550785dd026b0ae7b45f9cec079e8a4f142d1b9e
Calm-Waters <99409522+Calm-Waters@users.noreply.github.com>
2024-11-09 17:16:52 +0100

    Update login.js

--- modules/login.js
+++ modules/login.js
@@ -18,12 +18,6 @@ window.login_layout = `
 	</form>
 </div>
 `;
-const savedEmail = localStorage.getItem("email");
-const savedPassword = localStorage.getItem("password");
-if (savedEmail) { document.getElementById("email").value = savedEmail;}
-if (savedPassword) { document.getElementById("password").value = savedPassword;}
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-const auth = getAuth();
-function testfunct(){
-	console.log('here's hoping this works');
+function greetUser(name) {
+	console.log(`Hello, ${name}!`);
 }
