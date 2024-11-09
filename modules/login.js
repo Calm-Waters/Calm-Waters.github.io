22c6660fcccb7966cb6540a4969983e8e8f023f7
Calm-Waters <99409522+Calm-Waters@users.noreply.github.com>
2024-11-09 18:02:11 +0100

    Update login.js

--- modules/login.js
+++ modules/login.js
@@ -1,9 +1,9 @@
 const moduleName = "login";
 window[moduleName + '_layout'] = `
-    <div>
-        <h1>Welcome to the Example Module</h1>
-        <button id="printButton" onclick="printMessage()">Click me</button>
-    </div>
+<div>
+    <h1>Welcome to the Example Module</h1>
+    <button id="printButton" onclick="printMessage()">Click me</button>
+</div>
 `;
 window.printMessage = function() {
     console.log('Hello from the Example Module!');
