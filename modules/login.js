37f06a0b2285ed45ac73fecf64f9f65cfaf0d7b7
Calm-Waters <99409522+Calm-Waters@users.noreply.github.com>
2024-11-09 17:58:03 +0100

    Update login.js

--- modules/login.js
+++ modules/login.js
@@ -2,10 +2,9 @@ const moduleName = "login";
 window[moduleName + '_layout'] = `
     <div>
         <h1>Welcome to the Example Module</h1>
-        <button id="printButton">Click me to print a message</button>
+        <button id="printButton" onclick="printMessage()">Click me</button>
     </div>
 `;
 function printMessage() {
     console.log('Hello from the Example Module!');
 }
-document.getElementById('printButton')?.addEventListener('click', printMessage); 
